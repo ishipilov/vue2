46f584ee71d71a982b6c2914c07aed480f525509
Ivan Shipilov <ivan@ishipilov.ru>
2026-08-18 10:44:58 +0500

    ..

--- app/src/mixins/Mixin2.js
+++ app/src/mixins/Mixin2.js
@@ -1,26 +1,13 @@
 export default {
-  methods: {
-    capitalize: function (e) { return e.charAt(0).toUpperCase() + e.slice(1) },
-    markText: function (str,substr) {
-      let begin = str.toLowerCase().indexOf(substr.toLowerCase())
-      if (begin > -1) {
-        let endin = begin + substr.length
-        let bfore = str.substring(0, begin)
-        let markd = str.substring(begin, endin)
-        let after = str.substring(endin, str.length)
-        return bfore + '<mark class="px-0">' + markd + '</mark>' + after
-      } else {
-        return str
-      }
-    },
-    copyToClipboard: function (text) {
-      let textarea = document.createElement('textarea')
-      textarea.value = text
-      document.body.appendChild(textarea)
-      textarea.select()
-      let result = document.execCommand('copy')
-      document.body.removeChild(textarea)
-      return result
+  methods: {,
+		isUrl: function (str) {
+			let url
+			try { url = new URL(str) } catch (e) { return false }
+			return url.protocol === "http:" || url.protocol === "https:"
+		},
+    go2url: function (url, target) {
+      if (target === null || target === undefined) return window.open(url, '_self')
+      return window.open(url, target)
     }
   }
 };
